19b0a9c8ad258fac7dbc927936ee8ed0642a8df4
IvanMontesGuti <montesgutierrezivan@gmail.com>
2024-03-14 16:21:49 +0100

    Proyecto persona

--- Persona/src/model/Persona.java
+++ Persona/src/model/Persona.java
@@ -1,35 +1,16 @@
 package model;
 import java.util.*;
 
-/**
- * 
- */
 public class Persona {
 
-    /**
-     * Default constructor
-     */
+
     public Persona() {
     }
 
-    /**
-     * 
-     */
+
     private String dni;
-
-    /**
-     * 
-     */
     private String nombre;
-
-    /**
-     * 
-     */
     private String apellidos;
-
-    /**
-     * 
-     */
     private int edad;
 
     /**
@@ -39,75 +20,70 @@ public class Persona {
      * @param edad
      */
     public void Persona(String dni, String nombre, String apellidos, int edad) {
-        // TODO implement here
+        this.dni="4637573R";
+        this.nombre="lucario";
+        this.apellidos="morales";
+        this.edad=33;
     }
 
-    /**
-     * Modifica el DNI de la persona
+    
+     /* Modifica el DNI de la persona
      * @param dni
      */
     public void setDni(String dni) {
-        // TODO implement here
     }
-
-    /**
-     * Devuelve el DNI de la persona
+    /* Devuelve el DNI de la persona
      * @return
      */
     public String getDni() {
-        // TODO implement here
         return "";
     }
 
-    /**
-     * Modifica el nombre de la persona
+    /*Modifica el nombre de la persona
      * @param nombre
      */
     public void setNombre(String nombre) {
-        // TODO implement here
     }
 
-    /**
-     * Devuelve el nombre de la persona
+    /* Devuelve el nombre de la persona
      * @return
      */
     public String getNombre() {
-        // TODO implement here
-        return "";
+        return nombre;
     }
 
-    /**
-     * Modifica el apellido de la persona
+    /* Modifica el apellido de la persona
      * @param apellidos
      */
     public void setApellidos(String apellidos) {
-        // TODO implement here
     }
 
-    /**
-     * Devuelve el apellido de la persona
+    /*Devuelve el apellido de la persona
      * @return
      */
     public String getApellidos() {
-        // TODO implement here
-        return "";
+        return apellidos;
     }
 
-    /**
-     * Modifica la edad de la persona
+    /* Modifica la edad de la persona
      * @param edad
      */
     public void setEdad(int edad) {
-        // TODO implement here
     }
 
-    /**
-     * Devuelve la edad de la persona
+    /*Devuelve la edad de la persona
      * @return
      */
+    
     public int getEdad() {
-        // TODO implement here
-        return 0;
+        return edad;
     }
 
+    //Devuelve el contenido del constructor
+	@Override
+	public String toString() {
+		return "Persona [dni=" + dni + ", nombre=" + nombre + ", apellidos=" + apellidos + ", edad=" + edad + "]";
+	}
+
+    
 }
