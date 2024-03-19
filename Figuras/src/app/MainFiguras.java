f5827a1e233925ab926ce05708d7e9ac7a455ec6
IvanMontesGuti <montesgutierrezivan@gmail.com>
2024-03-19 16:52:18 +0100

    Proyecto Figuras

--- Figuras/src/app/MainFiguras.java
+++ Figuras/src/app/MainFiguras.java
@@ -1,5 +1,6 @@
 package app;
 
+import modelo.Circulo;
 import modelo.Rectangulo;
 
 public class MainFiguras {
@@ -9,10 +10,13 @@ public class MainFiguras {
 
 		
 		Rectangulo rectangulo1 = new Rectangulo (2,4);
+		Circulo circulo1 = new Circulo (3);
 		
 		
-		System.out.println("Perimetro: "+rectangulo1.Perimetro());
-		System.out.print("Area: "+rectangulo1.Area());
+		System.out.println("Perimetro de rectangulo: "+rectangulo1.Perimetro());
+		System.out.println("Area de rectangulo: "+rectangulo1.Area());
+		System.out.println("Perimetro del círculo: "+circulo1.getPerimetro());
+		System.out.println("Area del círculo: "+circulo1.getArea());
 	}
 
 }
---
+++ Figuras/src/modelo/Circulo.java
@@ -0,0 +1,75 @@
+package modelo;
+
+
+import java.util.*;
+
+public class Circulo {
+
+	private double radio;
+    private double pi = 3.14;
+	
+	
+    /**
+     * Default constructor
+     */
+    public Circulo() {
+    	
+    	this.radio =1;
+    	this.pi=3.14;
+    	
+    }
+
+
+    /**
+     * @param radio 
+     * @param pi
+     */
+    public Circulo(double radio) {
+    	
+    	this.radio = radio;
+    	this.pi = 3.14;
+        
+
+    }
+
+    public double getPi() {
+       
+    	return this.pi;
+    }
+
+    /**
+     * @param radio
+     */
+    public void setRadio(double radio) {
+        
+    	this.radio=radio;
+    }
+
+    public double getRadio() {
+    	
+    	return this.radio;
+        
+    }
+
+    /**
+     * @param radio 
+     * @param pi
+     */
+    public double getArea() {
+        
+    	return pi*(radio*radio);
+    	
+    	
+    }
+
+    /**
+     * @param radio 
+     * @param pi
+     */
+    public double getPerimetro() {
+    	
+    	return 2*pi*radio;
+        
+    }
+
+}
