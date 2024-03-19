d8d3d74cb756f30b4fc7e5f5bb0cb7e94268aaa2
IvanMontesGuti <montesgutierrezivan@gmail.com>
2024-03-19 17:09:43 +0100

    Proyecto Figuras

--- Figuras/src/app/MainFiguras.java
+++ Figuras/src/app/MainFiguras.java
@@ -2,6 +2,7 @@ package app;
 
 import modelo.Circulo;
 import modelo.Rectangulo;
+import modelo.Triangulo;
 
 public class MainFiguras {
 
@@ -11,12 +12,14 @@ public class MainFiguras {
 		
 		Rectangulo rectangulo1 = new Rectangulo (2,4);
 		Circulo circulo1 = new Circulo (3);
+		Triangulo triangulo1= new Triangulo (4,5);
 		
 		
 		System.out.println("Perimetro de rectangulo: "+rectangulo1.Perimetro());
 		System.out.println("Area de rectangulo: "+rectangulo1.Area());
 		System.out.println("Perimetro del círculo: "+circulo1.getPerimetro());
 		System.out.println("Area del círculo: "+circulo1.getArea());
+		System.out.println("Area del triangulo: "+triangulo1.getArea());
 	}
 
 }
---
+++ Figuras/src/modelo/Triangulo.java
@@ -0,0 +1,69 @@
+package modelo;
+
+
+import java.util.*;
+
+
+public class Triangulo {
+
+    /**
+     * Default constructor
+     */
+    public Triangulo() {
+    	this.base = 2;
+    	this.altura =2;
+    }
+
+    private double base;
+    private double altura;
+
+
+    /**
+     * @param base 
+     * @param altura
+     */
+    public  Triangulo(double base, double altura) {
+    	
+    	this.base =base;
+    	this.altura=altura;
+    }
+
+
+    public double getBase() {
+        
+    	return this.base;
+    }
+
+    /**
+     * @param base
+     */
+    public void setBase(int base) {
+        
+    	this.base=base;
+    }
+
+
+    public double getAltura() {
+        return this.altura;
+    }
+
+    /**
+     * @param altura
+     */
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+
+
+    /**
+     * @param base 
+     * @param altura
+     */
+    public double getArea() {
+    	
+    	return (base*altura)/2;
+        
+    }
+
+
+}
