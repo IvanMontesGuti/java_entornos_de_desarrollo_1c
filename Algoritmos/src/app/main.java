251e1fc0f63d4575324b54a6efa01c60e8d5a93c
IvanMontesGuti <montesgutierrezivan@gmail.com>
2024-04-23 17:10:32 +0200

    excepcion

--- Algoritmos/src/app/main.java
+++ Algoritmos/src/app/main.java
@@ -8,12 +8,14 @@ public class main {
 		// TODO Auto-generated method stub
 		
 		int s1 = Algoritmos.sumatorio(10);
-		int s2 = Algoritmos.sumatorio(0);
+		int s2 = Algoritmos.sumatorio(-1);
 		
 		int f1;
 		System.out.print(Algoritmos.factorial(4));
+		System.out.print(s2);
 		
 
+		
 	}
 
 }
--- Algoritmos/src/tests/testAlgoritmo.java
+++ Algoritmos/src/tests/testAlgoritmo.java
@@ -9,7 +9,7 @@ import util.Algoritmos;
 class testAlgoritmo {
 
 	@Test
-	void sumatorio() {
+	void sumatorio1() {
 		int sumaEsperada=10;
 		int sumaObtenida = Algoritmos.sumatorio(4);
 		assertEquals(sumaEsperada,sumaObtenida);
@@ -20,6 +20,23 @@ class testAlgoritmo {
 		int factorialEsperado=24;
 		int factorialObtenido = Algoritmos.factorial(4);
 		assertEquals(factorialEsperado,factorialObtenido);
+
+}
+	@Test
+	void sumatorioParamIncorrecto() {
+		Exception excepcion = assertThrows(IllegalArgumentException.class,
+				()->Algoritmos.sumatorio(-1));
+
+		String esperado = "El numero debe ser igual o superior a 0";
+				String obtenido = excepcion.getMessage();
+		assertEquals(esperado,obtenido);
+
+}
+	@Test
+	void sumatorioDe0() {
+		int sumaEsperada=0;
+		int sumaObtenida = Algoritmos.sumatorio(0);
+		assertEquals(sumaEsperada,sumaObtenida);
 
 }
 
